dfa2e79741b369aada11ca5e49a91b1bc26ffb8a
stormkirill98 <stormkirill98@gmail.com>
2020-03-24 23:15:40 +0300

    refactor: pick out id in const

--- src/main/kotlin/ClockWidget.kt
+++ src/main/kotlin/ClockWidget.kt
@@ -2,9 +2,11 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.impl.status.EditorBasedWidget
 
+const val CLOCK_WIDGET_ID = "ClockWidget"
+
 class ClockWidget(project: Project) : EditorBasedWidget(project) {
     override fun ID(): String {
-        return "ClockWidget"
+        return CLOCK_WIDGET_ID
     }
 
     override fun getPresentation(): StatusBarWidget.WidgetPresentation? {
